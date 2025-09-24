1818001f4054faed956cf2c6129458bdcbea5068
Akash20637 <akashvarshney209@gmail.com>
2025-09-25 00:19:10 +0530

    Express app setup

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import dotenv from 'dotenv'
-import express from 'express'
+import app from './app.js'
 import connectDB from './db/db.js'
-
-//Create Express 
-const app = express()
 
 //Setup Environment Variable
 dotenv.config()
 
-//Connect DB
-connectDB()
-
-app.get('/', (req, res)=>{
-    res.send("Server is Running...")
-})
-
-
 // Running Server
 const port = process.env.PORT || 5000
-app.listen(port, ()=>{
-    console.log("Server is running on port : ", port)
+
+//Connect DB
+connectDB()
+.then(()=>{
+    app.listen(port, ()=>{
+        console.log("Server is running on port : ", port)
+    })
 })
